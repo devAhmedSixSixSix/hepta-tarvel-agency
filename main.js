4948e0fb2aeda9e585449d3914a4c8f6991f31d5
Dev Ahmed <95584087+devAhmedSixSixSix@users.noreply.github.com>
2023-02-27 09:32:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// for the navbar jump in animation
+const navbar = document.querySelector(".navbar");
+const landing = document.querySelector(".header");
+
+const navbarObserver = new IntersectionObserver(
+  (entres) => {
+    entres.forEach((entry) => {
+      navbar.classList.toggle("nav-scroll", !entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.4,
+  }
+);
+
+navbarObserver.observe(landing);
+
+// for the fade in animation in the page
+const items = document.querySelectorAll(".fade-in");
+
+const pageObserver = new IntersectionObserver(
+  (entres) => {
+    entres.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+      if (entry.isIntersecting) pageObserver.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 0.4,
+  }
+);
+
+items.forEach((item) => {
+  pageObserver.observe(item);
+});
